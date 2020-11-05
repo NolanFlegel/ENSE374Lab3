f429cdba4d3d0ac7ae176abb0e16c6e8cce03169
ParanoidMarvin0 <33326713+ParanoidMarvin0@users.noreply.github.com>
2020-11-05 16:47:54 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
 const app = express()
 const port = 5000
 
 
-const validateUser = require('./users.json')
-
+//setting static files
 app.use(express.static('public'))
-app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/css', express.static(__dirname + '\css'))
+
+//Templates
+app.use(expressLayouts)
+app.set('view engine', 'ejs')
+
 
 // Routes
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Login'})
+    res.render('index')
 })
 
 // Listen on Port 5000
 app.listen(port, () => console.info(`App listening on port ${port}`))
+
+
+//const validateUser = require('./users.json')
