a19a3bab46b26ef44fc619a7d8ba8c1fd3f9b20a
ParanoidMarvin0 <33326713+ParanoidMarvin0@users.noreply.github.com>
2020-11-05 16:39:07 -0600

    getting views setup

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express')
-
 const app = express()
 const port = 5000
 
+
+const validateUser = require('./users.json')
+
 app.use(express.static('public'))
-app.use('/')
+app.use('/css', express.static(__dirname + 'public/css'))
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Login'})
+})
+
+// Listen on Port 5000
+app.listen(port, () => console.info(`App listening on port ${port}`))
